eaa8ba7d20e02431aaacebb13115cd4d638b7632
Charlotte B <cburggie@gmail.com>
2020-05-07 15:03:17 -0700

    adjusted generation update rules

--- source/Dish.cpp
+++ source/Dish.cpp
@@ -9,28 +9,13 @@ static const Uint32 palette[4] = {
 	0xfff0f0f0
 };
 
-// sum of values in adjacent cells
-//  0 -  2:  -  1
-//  3 -  5: +/- 0
-//  6 -  8:  +  1
-//  9 - 11: +/- 0
-// 12 - 14:  -  1
-/*
 static const int val_map[4][25] = {
-	{0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{0,0,0,1,1,1,2,2,2,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
-	{1,1,1,2,2,2,3,3,3,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1},
-	{2,2,2,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2}
+	//0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4
+	{ 0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{ 0,0,0,0,0,0,1,1,1,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{ 1,1,1,1,1,1,2,2,2,3,3,3,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{ 2,2,2,2,2,2,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2}
 };
-*/
-static int val_map(int a, int b)
-{
-	if (b < 3) return a > 0 ? a - 1 : 0;
-	if (b < 6) return a;
-	if (b < 9) return a < 3 ? a + 1 : 3;
-	if (b < 12) return a;
-	return a > 0 ? a - 1 : 0;
-}
 
 static const int offset_1 = -1 - (petri::constants::width_in_cells + 2);
 static const int offset_2 =    - (petri::constants::width_in_cells + 2);
@@ -125,30 +110,7 @@ int petri::Dish::tick()
 		for (int x = 1; x < (1 + constants::width_in_cells); x++)
 		{
 			int pos = y * buf_width + x;
-			/*
-			std::cout << "updating cell (" << x << ',' << y << "), offset ";
-			std::cout << pos << std::endl;
-
-			int map_y = buf1[pos];
-			std::cout << "val_map row: " << map_y << std::endl;
-
-			int map_x = buf1[pos + offset_1] +
-				buf1[pos + offset_2] +
-				buf1[pos + offset_3] +
-				buf1[pos + offset_4] +
-				buf1[pos + offset_5] +
-				buf1[pos + offset_6] +
-				buf1[pos + offset_7] +
-				buf1[pos + offset_8];
-			std::cout << "val_map col: " << map_x << std::endl;
-
-			int map_val = val_map[map_y][map_x];
-			std::cout << "val_map value: " << map_val << std::endl;
-
-			buf2[pos] = map_val;
-			*/
-			//buf2[pos] = val_map[buf1[pos]][
-			buf2[pos] = val_map(buf1[pos],(
+			buf2[pos] = val_map[buf1[pos]][
 				buf1[pos + offset_1] +
 				buf1[pos + offset_2] +
 				buf1[pos + offset_3] +
@@ -157,12 +119,9 @@ int petri::Dish::tick()
 				buf1[pos + offset_6] +
 				buf1[pos + offset_7] +
 				buf1[pos + offset_8]
-			//];
-				));
+			];
 		}
 	}
-
-	//std::cout << "updating cells done" << std::endl;
 
 	int * buftmp = buf1;
 	buf1 = buf2;
@@ -185,13 +144,6 @@ int petri::Dish::draw()
 
 		for (int x = 0; x < surface->w; x++)
 		{
-		/*
-			int bo = buffer_offset+(x/constants::cell_size_in_pixels);
-			int so = surface_offset + x;
-			int po = buf1[bo];
-			int color = palette[po];
-			sur_buf[so] = color;
-		*/
 			sur_buf[surface_offset + x] = palette[buf1[buffer_offset+(x/constants::cell_size_in_pixels)]];
 		}
 	}
